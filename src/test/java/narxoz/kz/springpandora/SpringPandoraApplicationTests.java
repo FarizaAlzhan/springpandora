c45be0320d0bac77da8324feed001f6523eb6613
FarizaAlzhan <fariza.alzhan@narxoz.kz>
2022-11-12 15:07:29 +0600

    spring

---
+++ src/test/java/narxoz/kz/springpandora/SpringPandoraApplicationTests.java
@@ -0,0 +1,13 @@
+package narxoz.kz.springpandora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPandoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
