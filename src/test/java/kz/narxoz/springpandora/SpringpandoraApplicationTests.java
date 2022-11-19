b8d657fb7fe3bc9d990ccd98cc669ab165c38e1c
Asem Syrymbetova <assem.syrymbetova@narxoz.kz>
2022-11-19 15:14:32 +0600

    git init

---
+++ src/test/java/kz/narxoz/springpandora/SpringpandoraApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.springpandora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpandoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
